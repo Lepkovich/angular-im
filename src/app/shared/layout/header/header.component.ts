a42629b5d0b36134a26f5d1e58738b2c580e34da
Lepkovich <6332133@gmail.com>
2023-12-17 14:30:58 +0300

    header and footer done. lesson 1 finish

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CategoryType} from "../../../../types/category.type";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  @Input() categories: CategoryType[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from "../services/category.service";
+import {CategoryType} from "../../../types/category.type";
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
 
+  categories: CategoryType[] = [];
+
+  constructor(private categoryService: CategoryService) {
+  }
+
+  ngOnInit() {
+    this.categoryService.getCategories().
+    subscribe((categories: CategoryType[]) => {
+      this.categories = categories;
+    })
+  }
 }
