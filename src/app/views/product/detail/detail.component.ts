8f3731a00f853842070b998383e680c6d9043042
Lepkovich <6332133@gmail.com>
2024-01-03 22:50:30 +0300

    lesson 6 done. detail-component works

--- src/app/views/product/detail/detail.component.ts
+++ src/app/views/product/detail/detail.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OwlOptions} from "ngx-owl-carousel-o";
+import {ProductType} from "../../../../types/product.type";
+import {ProductService} from "../../../shared/services/product.service";
+import {ActivatedRoute} from "@angular/router";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit{
+
+  count: number = 1;
+  recommendedProducts: ProductType[] = [];
+  product!: ProductType;
+  serverStaticPath = environment.serverStaticPath;
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 24,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: false
+  }
+
+  constructor(private productService: ProductService,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.productService.getProduct(params['url'])
+        .subscribe((data: ProductType) => {
+          this.product = data;
+        })
+      })
+
+    this.productService.getBestProducts()
+      .subscribe((data: ProductType[]) => {
+        this.recommendedProducts = data;
+      })
+  }
+
+  updateCount(value: number) {
+    this.count = value;
+
+  }
+
+  addToCart() {
+    alert('Добавлено в корзину: ' + this.count)
+  }
 
 }
