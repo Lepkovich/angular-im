f2d7de6a19b91cca6cfb79610a85859506bb6a15
Lepkovich <6332133@gmail.com>
2024-01-08 17:34:48 +0300

    45m of lesson

--- src/app/views/order/order/order.component.ts
+++ src/app/views/order/order/order.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../../shared/services/cart.service";
+import {CartType} from "../../../../types/cart.type";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {DeliveryType} from "../../../../types/delivery.type";
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
 
+  deliveryType: DeliveryType = DeliveryType.delivery;
+  deliveryTypes = DeliveryType;
+
+  cart: CartType | null = null;
+  totalAmount: number = 0;
+  totalCount: number = 0;
+
+  constructor(private cartService: CartService,
+              private router: Router,
+              private _snackBar: MatSnackBar,) {
+  }
+
+  ngOnInit() {
+    this.cartService.getCart()
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+        this.cart = (data as CartType);
+        if (!this.cart || (this.cart && this.cart.items.length === 0)) {
+          this._snackBar.open('Корзина пустая')
+          this.router.navigate(['/']);
+          return;
+        }
+        this.calculateTotal();
+      })
+  }
+
+  calculateTotal() {
+    this.totalAmount = 0;
+    this.totalCount = 0;
+
+    if (this.cart) {
+      this.cart.items.forEach(item => {
+        this.totalAmount += item.quantity * item.product.price;
+        this.totalCount += item.quantity;
+      })
+    }
+  }
+
+  changeDeliveryType(type: DeliveryType) {
+    this.deliveryType = type;
+  }
 }
