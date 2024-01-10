5fd10f263977a5ed4ec096d24901ec8dfdc20173
Lepkovich <6332133@gmail.com>
2024-01-10 08:53:14 +0300

    28m of lesson

--- src/app/views/personal/info/info.component.ts
+++ src/app/views/personal/info/info.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {PaymentType} from "../../../../types/payment.type";
+import {DeliveryType} from "../../../../types/delivery.type";
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit{
 
+  deliveryType: DeliveryType = DeliveryType.delivery;
+  deliveryTypes = DeliveryType;
+  paymentTypes = PaymentType;
+
+  userInfoForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    fatherName: [''],
+    phone: [''],
+    paymentType: [PaymentType.cashToCourier],
+    email: ['', Validators.email],
+    street: [''],
+    house: [''],
+    entrance: [''],
+    apartment: ['']
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+  }
+
+  changeDeliveryType(type: DeliveryType) {
+    this.deliveryType = type;
+    this.userInfoForm.markAsDirty()
+   }
+
+  updateUserInfo() {
+
+  }
 }
